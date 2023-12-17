7640fe3204c347af17836fe90e09e472042cd7b2
Dyst <50362174+Dyastinn@users.noreply.github.com>
2023-12-17 19:40:44 +0800

    Created Git

---
+++ Data/Migrations/20231215183401_pokemonSetup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokedexWebApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class pokemonSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pokemon",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Total = table.Column<int>(type: "int", nullable: false),
+                    HP = table.Column<int>(type: "int", nullable: false),
+                    Attack = table.Column<int>(type: "int", nullable: false),
+                    Defense = table.Column<int>(type: "int", nullable: false),
+                    SPAttack = table.Column<int>(type: "int", nullable: false),
+                    SPDefense = table.Column<int>(type: "int", nullable: false),
+                    Speed = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pokemon", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pokemon");
+        }
+    }
+}
---
+++ Models/Pokemon.cs
@@ -0,0 +1,20 @@
+﻿namespace PokedexWebApp.Models;
+
+public class Pokemon
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Type1 { get; set; }
+    public string? Type2 { get; set; }
+    public int Total { get; set; }
+    public int HP { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+    public int SPAttack { get; set; }
+    public int SPDefense { get; set; }
+    public int Speed { get; set; }
+    public Pokemon()
+    {
+
+    }
+}
